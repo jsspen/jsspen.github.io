f12440bd05fcd171b1ed14b5cc0c78c3528c375d
Jordan Spencer <Jordan.S.Spencer@gmail.com>
2024-12-13 10:01:52 -0500

    checkbox rendering added

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,12 +2,16 @@ import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 import pluginPDFEmbed from "eleventy-plugin-pdfembed";
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import { full } from "markdown-it-emoji/index.mjs";
+import MarkdownItTaskCheckbox from "markdown-it-task-checkbox/index.js";
 
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ public: "/" });
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(full));
+  eleventyConfig.amendLibrary("md", (mdLib) =>
+    mdLib.use(MarkdownItTaskCheckbox)
+  );
   eleventyConfig.addPlugin(pluginPDFEmbed, {
     key: "3545089f15d94af3a4661706590b5591",
   });
